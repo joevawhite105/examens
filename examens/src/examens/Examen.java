4954aa05350dd750ea4975b27cf82caff20062c7
jaspe <jaspe@192.168.0.172>
2024-05-30 16:38:52 +0200

    initial commit

---
+++ examens/src/examens/Examen.java
@@ -0,0 +1,57 @@
+package examens;
+
+import java.util.HashSet;
+import java.util.Set;
+import logicalcollections.LogicalSet;
+/**
+ * @invar | getLokalen() != null
+ * @invar | getLokalen().stream().allMatch(l -> l != null && l.getExamens().contains(this))
+ */
+public class Examen {
+	/**
+	 * @peerObjects
+	 * @representationObject
+	 * @invar | lokalen != null
+     * @invar | lokalen.stream().allMatch(l -> l != null && l.examens.contains(this))
+	 */
+	 Set<Lokaal> lokalen = new HashSet<>();
+	
+	Set<Lokaal> getLokalenInternal(){
+		return Set.copyOf(lokalen);
+	}
+	/**
+     * @post | getLokalen().isEmpty()
+     */
+	public Examen() {}
+	/**
+	 * @peerObjects
+	 * @creates | result
+	 */
+	public Set<Lokaal> getLokalen() {return Set.copyOf(lokalen);}
+	/**
+	 * @pre | lok != null
+	 * 
+	 * @mutates_properties | getLokalen(), lok.getExamens()
+	 * 
+	 * @post | getLokalen().equals(LogicalSet.plus(old(getLokalen()),lok))
+	 * @post | lok.getExamens().equals(LogicalSet.plus(old(lok.getExamens()),this))
+	 */
+	public void addLokaal(Lokaal lok) {
+		lokalen.add(lok);
+		lok.examens.add(this);
+	}
+	/**
+	 * @pre | lok != null
+	 * @pre | getLokalen().contains(this)
+	 * 
+	 * @mutates_properties | getLokalen(), lok.getExamens()
+	 * 
+	 * @post | getLokalen().equals(LogicalSet.minus(old(getLokalen()),lok))
+	 * @post | lok.getExamens().equals(LogicalSet.minus(old(lok.getExamens()),this))
+	 */
+	public void deleteLokaal(Lokaal lok) {
+		lok.examens.remove(this);
+		lokalen.remove(lok);
+	}
+
+}
---
+++ examens/src/examens/Lokaal.java
@@ -0,0 +1,56 @@
+package examens;
+
+import java.util.HashSet;
+import java.util.Set;
+import logicalcollections.LogicalSet;
+/**
+ * @invar | getExamens() != null
+ * @invar | getExamens().stream().allMatch(e -> e != null && e.getLokalen().contains(this))
+ */
+public class Lokaal {
+	/**
+	 * @representationObject
+	 * @peerObjects
+	 * @invar | examens != null
+	 * @invar | examens.stream().allMatch(e -> e != null && e.lokalen.contains(this))
+	 */
+	 Set<Examen> examens = new HashSet<>(); 
+    /**
+     * @post | getExamens().isEmpty()
+     */
+	
+	Set<Examen> getExamensInternal(){
+		return Set.copyOf(examens);
+	}
+	public Lokaal() {}
+	/**
+	 * @peerObjects
+	 * @creates | result
+	 */
+	public Set<Examen> getExamens() {return Set.copyOf(examens);}
+//	/**
+//	 * @pre | exam != null
+//	 * @pre | !getExamens().contains(this)
+//	 * 
+//	 * @mutates_properties | getExamens(), exam.getLokalen()
+//	 * 
+//	 * @post | getExamens().equals(LogicalSet.plus(old(getExamens()),exam))
+//	 * @post | exam.getLokalen().equals(LogicalSet.plus(old(exam.getLokalen()),this))
+//	 */
+//	void addExamen(Examen exam) {
+//		examens.add(exam);
+//		
+//		
+//	}
+//	/**
+//	 * @pre | exam != null
+//	 * @pre | getExamens().contains(this)
+//	 * 
+//	 * @mutates_properties | getExamens(), exam.getLokalen()
+//	 * 
+//	 * @post | getExamens().equals(LogicalSet.minus(old(getExamens()),exam))
+//	 * @post | exam.getLokalen().equals(LogicalSet.minus(old(exam.getLokalen()),this))
+//	 */
+//	 void deleteExamen(Examen exam) {throw new RuntimeException("nee");}
+
+}
